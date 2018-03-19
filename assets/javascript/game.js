47ec984817143ac765d03114abc807ebc483728f
Andrea Velardi <andreavelardi@Andreas-MacBook-Pro.local>
2018-03-18 21:48:57 -0500

    changes 3.18.2018v3

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//PseduCode 
+//show random number at start of game
+//if image is clicked then add points to the total score
+//the images should all have a random amount of values 
+//when th eimage is clicked again, update the points by adding image clicked points to current total points
+//if the total score is equal to the random number then win! or else if the total score is > # then lose
+//also remember to keep track of wins and losses and auto restart w/out refreshing page
+//the random # is between 19-120
+//and each sugar skull should have value of 1-12 that is hidden
